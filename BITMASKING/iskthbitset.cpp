7a80d2e3fdb6280734bb55bd66ce3742d5399905
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-09-26 22:55:54 +0530

    Create iskthbitset.cpp

---
+++ BITMASKING/iskthbitset.cpp
@@ -0,0 +1,19 @@
+ #include <iostream> 
+using namespace std; 
+
+void isKthBitSet(int n, int k) 
+{ 
+	if (n & (1 << (k - 1))) 
+		cout << "SET"; 
+	else
+		cout << "NOT SET"; 
+} 
+
+// Driver code 
+int main() 
+{ 
+int n,k;
+cin>>n>>k;
+isKthBitSet(n, k); 
+	return 0;   
+} 
